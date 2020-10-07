d6dc89d6e4329d62f5af0777481f3073d0923b9d
shaikadilmd <shaikadilmd@gmail.com>
2020-10-06 22:35:56 -0400

    Implemented CORS

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const loadData = require("./LoadData")
+const cors= require('cors')
 const app = express();
 const port = 3000;
 
-app.use('/',express.static('public'));
+app.use(cors());
 
 
 const budget = {
@@ -26,14 +27,12 @@ const budget = {
 };
 
 
-app.get('/hello',(req,res) => {
-    res.send('Hello World!');
-});
+
 
 app.get('/budget',(req,res) => {
     res.json(loadData);
 });
 
 app.listen(port, () => {
-    console.log('Example app listening at:+ http://localhost:${port}');
+    console.log(`Example app listening at:+ http://localhost:${port}`);
 });
